a122f52644484afcebac8112fb39ee1bb07580b7
Curt House <66846507+Neurum@users.noreply.github.com>
2021-03-11 08:35:37 -0500

    deploy config changes

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
 module.exports = {
   publicPath:
-    process.env.NODE_ENV === 'production' ? '/personal-site-2.0/' : '/',
+    process.env.NODE_ENV === 'production' ? '/Neurum.github.io/' : '/',
 };
